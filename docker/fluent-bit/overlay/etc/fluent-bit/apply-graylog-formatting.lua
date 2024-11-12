a530b944a2de9b85d78f43f3c39ab918bdfd14ec
Josh.5 <jsunnex@gmail.com>
2024-11-13 09:40:30 +1300

    Another attempt to fix the bug where some `source.` keys are not renamed to `source_`
    This moves the loop to the top of the function and creates a whole new `new_record` object. Perhaps the issue is that we are modifying the record while looping over it??? This should fix that if that were the case.

--- docker/fluent-bit/overlay/etc/fluent-bit/apply-graylog-formatting.lua
+++ docker/fluent-bit/overlay/etc/fluent-bit/apply-graylog-formatting.lua
@@ -4,46 +4,61 @@
 --File Created: Tuesday, 29th October 2024 3:18:29 pm
 --Author: Josh5 (jsunnex@gmail.com)
 -------
---Last Modified: Tuesday, 12th November 2024 5:29:36 pm
+--Last Modified: Wednesday, 13th November 2024 9:39:05 am
 --Modified By: Josh5 (jsunnex@gmail.com)
 --]]
 
 
 function graylog_formatting(tag, timestamp, record)
+    -- Create a new record
+    local new_record = {}
+    for key, value in pairs(record) do
+        print(key, value)
+        new_record[key] = value
+
+        -- Convert any "source." keys to "source_". Graylog will do this already, but lets do it here so anything
+        --  sent to another index not managed by Graylog will be uniform.
+        if key:sub(1, 7) == "source." then
+            local new_key = "source_" .. key:sub(8)
+            new_record[new_key] = value
+            new_record[key] = nil  -- Remove the original "source." key
+        end
+    end
+    
     -- Check if "short_message" exists and is a non-empty string
-    if record["short_message"] then
-        if type(record["short_message"]) == "string" and record["short_message"] == "" then
-            record["short_message"] = nil  -- Remove if it's an empty string
+    if new_record["short_message"] then
+        if type(new_record["short_message"]) == "string" and new_record["short_message"] == "" then
+            new_record["short_message"] = nil  -- Remove if it's an empty string
         end
     end
 
     -- Ensure "message" exists; if not, create one with "NO MESSAGE" or from "log"
-    if not record["message"] or (type(record["message"]) ~= "string" or record["message"] == "") then
-        if record["log"] and (type(record["log"]) == "string" and record["log"] ~= "") then
-            record["message"] = record["log"]   -- Use log if it exists and is not empty
-            record["log"] = nil                 -- Remove "log" after transferring its value
+    if not new_record["message"] or (type(new_record["message"]) ~= "string" or new_record["message"] == "") then
+        if new_record["log"] and (type(new_record["log"]) == "string" and new_record["log"] ~= "") then
+            new_record["message"] = new_record["log"]   -- Use log if it exists and is not empty
+            new_record["log"] = nil                 -- Remove "log" after transferring its value
         else
-            record["message"] = "NO MESSAGE"    -- Default to "NO MESSAGE"
+            new_record["message"] = "NO MESSAGE"    -- Default to "NO MESSAGE"
         end
     end
 
     -- Ensure "source" exists; if not, create one with "unknown" or from tag
-    if not record["source"] or (type(record["source"]) ~= "string" or record["source"] == "") then
+    if not new_record["source"] or (type(new_record["source"]) ~= "string" or new_record["source"] == "") then
         if tag and (type(tag) == "string" and tag ~= "") then
-            record["source"] = tag          -- Use tag if it exists and is not empty
+            new_record["source"] = tag          -- Use tag if it exists and is not empty
         else
-            record["source"] = "unknown"    -- Default to "unknown"
+            new_record["source"] = "unknown"    -- Default to "unknown"
         end
     end
 
     -- Check if "timestamp" exists; if not, use the provided timestamp from Fluent Bit
-    if not record["timestamp"] then
-        record["timestamp"] = timestamp     -- Use Fluent Bit timestamp
+    if not new_record["timestamp"] then
+        new_record["timestamp"] = timestamp     -- Use Fluent Bit timestamp
     end
 
     -- Check and convert "level" to syslog standard int
-    if record["level"] then
-        local level_string = record["level"]:gsub("^%s*(.-)%s*$", "%1")  -- Trim whitespace
+    if new_record["level"] then
+        local level_string = new_record["level"]:gsub("^%s*(.-)%s*$", "%1")  -- Trim whitespace
         local level_map = {
             emergency = 0,
             alert = 1,
@@ -62,34 +77,24 @@ function graylog_formatting(tag, timestamp, record)
 
         -- Check if the lowercased level string exists in the map
         if level_map[lower_level_string] then
-            record["level"] = level_map[lower_level_string]     -- Set level to the corresponding integer
-            record["levelname"] = level_string                  -- Move the original level string to "levelname"
+            new_record["level"] = level_map[lower_level_string]     -- Set level to the corresponding integer
+            new_record["levelname"] = level_string                  -- Move the original level string to "levelname"
         else
-            record["level"] = 6                                 -- Default to Info if level is unrecognized
-            record["levelname"] = level_string                  -- Move original level string to "levelname"
-        end
-    end
-
-    -- Convert any "source." keys to "source_". Graylog will do this already, but lets do it here so anything
-    --  sent to another index not managed by Graylog will be uniform.
-    for key, value in pairs(record) do
-        if key:sub(1, 7) == "source." then
-            local new_key = "source_" .. key:sub(8)
-            record[new_key] = value
-            record[key] = nil  -- Remove the original "source." key
+            new_record["level"] = 6                                 -- Default to Info if level is unrecognized
+            new_record["levelname"] = level_string                  -- Move original level string to "levelname"
         end
     end
 
     -- Ensure "service_name" exists. 
     -- A service name is something required by Open Telemetry semantic conventions. Grafana will automatically apply this as "fluent-bit" if it does not exist.
-    if not record["service_name"] or (type(record["service_name"]) ~= "string" or record["service_name"] == "") then
-        if record["source_service"] and (type(record["source_service"]) == "string" and record["source_service"] ~= "") then
-            record["service_name"] = record["source_service"]   -- Use "source_service" record if it exists and is not empty
+    if not new_record["service_name"] or (type(new_record["service_name"]) ~= "string" or new_record["service_name"] == "") then
+        if new_record["source_service"] and (type(new_record["source_service"]) == "string" and new_record["source_service"] ~= "") then
+            new_record["service_name"] = new_record["source_service"]   -- Use "source_service" record if it exists and is not empty
         else
-            record["service_name"] = record["source"]           -- Default to "source" record
+            new_record["service_name"] = new_record["source"]           -- Default to "source" record
         end
     end
 
-    -- Return the modified record
-    return 1, timestamp, record
+    -- Return the modified new_record
+    return 1, timestamp, new_record
 end
